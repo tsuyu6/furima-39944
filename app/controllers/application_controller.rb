70c14a9b14e742abf6cffe5e74f370a3adc897ca
tsuyu6 <tsuyu.tsuyu.0606@gmail.com>
2024-03-07 20:42:59 +0900

    ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:first_name_kana,:family_name_kana,:nickname,:birthday])
+  end
 
   def basic_auth
+    
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
--- db/migrate/20240306105107_devise_create_users.rb
+++ db/migrate/20240306105107_devise_create_users.rb
@@ -3,7 +3,13 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :nickname, null: false
       ## Database authenticatable
+      t.string :family_name,null: false
+      t.string :first_name,null: false
+      t.string :first_name_kana,null: false
+      t.string :family_name_kana,null: false
+      t.date :birthday,null: false 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
