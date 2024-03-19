adf18e68af016699cf4ed11ce3eb99b6f591479a
tsuyu6 <tsuyu.tsuyu.0606@gmail.com>
2024-03-19 21:29:04 +0900

    商品一覧

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new]
-  def index
-    
+  def index 
+    @items = Item.order("created_at DESC")
   end
   def new
     @item = Item.new
