1c27e95b66daa46dee665d71a0def34154e7c3f4
tsuyu6 <tsuyu.tsuyu.0606@gmail.com>
2024-03-23 20:53:33 +0900

    商品詳細機能表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only: [:new]
+before_action :authenticate_user!,only: [:new]
   def index 
     @items = Item.order("created_at DESC")
   end
@@ -8,14 +8,20 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+  @item = Item.new(item_params)
     if @item.save
-      # 保存できたらトップページに遷移する。
-      redirect_to root_path
+    # 保存できたらトップページに遷移する。
+    redirect_to root_path
     else
-      render :new,status: :unprocessable_entity
-    end
-  end 
+    render :new,status: :unprocessable_entity
+  end
+end
+
+  def show
+        @item = Item.find(params[:id])
+
+  end
+
   private
   def item_params
     params.require(:item).permit(:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:item_name,:price,:information,:delively_day_id,:image).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: "items#index"
-  resources :items, only: [:new, :create,]
+  root to: "items#index" 
+  resources :items, only: [:new, :create,:show,]
 end
