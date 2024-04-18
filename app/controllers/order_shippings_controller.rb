30619880eaea705243c51424e4f9faf06974414b
tsuyu6 <tsuyu.tsuyu.0606@gmail.com>
2024-04-18 21:37:59 +0900

    商品購入機能、6回目

--- app/controllers/order_shippings_controller.rb
+++ app/controllers/order_shippings_controller.rb
@@ -2,13 +2,11 @@ class OrderShippingsController < ApplicationController
   before_action :authenticate_user!,only: [:index, :create]
   before_action :set_item, only:[:index ,:create]
   def set_item
-    @item = Item.find(params[:item_id])
    end
  
  
   def index 
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @item = Item.find(params[:item_id])
     if current_user.id == @item.user_id || @item.order.present?
       redirect_to root_path
     end
@@ -16,7 +14,6 @@ class OrderShippingsController < ApplicationController
   end
 
   def create 
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_shipping_params) 
     if @order_shipping.valid?
       pay_item
@@ -31,6 +28,7 @@ class OrderShippingsController < ApplicationController
   
 
     private
+    @item = Item.find(params[:item_id])
 
   def order_shipping_params
     params.require(:order_shipping).permit(:post_code, :prefecture_id, :city, :block_number, :building_name, :telephone_number).merge(user_id: current_user.id,item_id:@item.id,token: params[:token])
